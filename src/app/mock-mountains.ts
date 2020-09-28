6d051796d457b7874900e57f9e1d226aa71a1905
TonyChou <cool21540125@gmail.com>
2020-09-28 15:34:03 +0800

    Chap2

---
+++ src/app/mock-mountains.ts
@@ -0,0 +1,7 @@
+import { Mountain } from './mountain';
+
+export const MOUNTAINS: Mountain[] = [
+  { id: 'D001', name: '中央尖山' },
+  { id: 'D090', name: '志佳陽大山' },
+  { id: 'D080', name: '玉山' }
+];
--- src/app/mountain/mountain.component.ts
+++ src/app/mountain/mountain.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Mountain } from '../mountain';
+import { MOUNTAINS } from '../mock-mountains';
 
 @Component({
   selector: 'app-mountain',
@@ -8,14 +9,16 @@ import { Mountain } from '../mountain';
 })
 export class MountainComponent implements OnInit {
 
-  mountain: Mountain = {
-    id: 'D001',
-    name: '中央尖山'
-  };
+  mountains = MOUNTAINS;
+  selectedMt: Mountain;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(mt: Mountain): void {
+    this.selectedMt = mt;
   }
 
 }
